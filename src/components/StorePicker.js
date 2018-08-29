194df8421556a38f5051f2a936a0c916e009d40d
bouncingAlien <dejan.bl@gmail.com>
2018-08-29 15:41:51 +0200

    Created router; Create App component; Create Header Component; Create Inventory Component; Create NotFoud Component; Create Order Component;

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,26 @@
 import React from 'react';
-
+import { getFunName } from '../helpers';
+ 
 class StorePicker extends React.Component {
+    constructor() {
+        super();
+        this.goToStore = this.goToStore.bind(this);
+    }
+    myInput = React.createRef();
+    goToStore(e) {
+        e.preventDefault();
+        this.props.history.push(`/store/${this.myInput.current.value}`);
+    }
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={ this.goToStore }>
                 <h2>Please Enter A Store</h2>
-                <input type="text" required placeholder="Store Name" />
+                <input 
+                    type="text" 
+                    ref= { this.myInput }
+                    required placeholder="Store Name" 
+                    defaultValue={ getFunName() }
+                />
                 <button type="submit">Visit Store &rarr;</button>
             </form>
         )
