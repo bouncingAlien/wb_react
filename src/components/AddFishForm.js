9a95c44c2280a0bcb1e152afa3e67da834786adc
bouncingAlien <dejan.bl@gmail.com>
2018-08-30 18:14:00 +0200

    Create input form; Created method to update state from input form;

---
+++ src/components/AddFishForm.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+class AddFishForm extends React.Component {
+    constructor(){
+        super();
+        this.createFish = this.createFish.bind(this);
+    }
+    nameRef = React.createRef();
+    priceRef = React.createRef();
+    statusRef = React.createRef();
+    descRef = React.createRef();
+    imageRef = React.createRef();
+    createFish(e){
+        e.preventDefault();
+        const fish = {
+            name: this.nameRef.current.value,
+            price: parseFloat(this.priceRef.current.value),
+            status: this.statusRef.current.value,
+            desc: this.descRef.current.value,
+            image: this.imageRef.current.value
+        }
+        this.props.addFish(fish);
+        e.currentTarget.reset();
+    }
+    render() {
+        return (
+            <form className="fish-edit" onSubmit={this.createFish}>
+                <input type="text" name="name" placeholder="Name" ref={this.nameRef} />
+                <input type="text" name="price" placeholder="Price" ref={this.priceRef} />
+                <select name="status" ref={this.statusRef}>
+                    <option value="available">Fresh!</option>
+                    <option value="unavailable">Sold Out!</option>
+                </select>
+                <textarea name="desc" placeholder="Description" ref={this.descRef}></textarea>
+                <input type="text" name="image" placeholder="Image" ref={this.imageRef} />
+                <button type="submit"> + Add Fish</button>
+            </form>
+        )
+    }
+}
+
+export default AddFishForm;
--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,31 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            fishes: {},
+            orders: {}
+        }
+        this.addFish = this.addFish.bind(this);
+        this.loadSampleFishes = this.loadSampleFishes.bind(this);
+    }
+    // custom methods
+    addFish(fish){
+        // take copy of existing state
+        const fishes = { ...this.state.fishes };
+        // add new element to variable
+        fishes[`fish${Date.now()}`] = fish;
+        // set state to value of variable
+        this.setState({ fishes });
+    }
+    loadSampleFishes(){
+        this.setState({ fishes: sampleFishes });
+    }
+    // render method -----------------------------------------
     render() {
         return (
             <div className="catch-of-the-day">
@@ -11,7 +34,7 @@ class App extends React.Component {
                     <Header tagline="fresh seefood market" />
                 </div>
                 <Order />
-                <Inventory />
+                <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
             </div>
         )
     }
